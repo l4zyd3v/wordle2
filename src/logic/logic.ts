6c802e1962a3c5a35079e0b6a54cef9c006723a5
l4zyd3v <nielsen.177@gmail.com>
2024-12-11 14:58:45 +0100

    yes

--- src/logic/logic.ts
+++ src/logic/logic.ts
@@ -35,5 +35,43 @@ async function insertAnswerInRow(answer: AnswerType, letter: string) {
   if (!letter) {
     throw new Error("Something went wrong - can't insert answer to tile");
   }
-  answer.push(letter);
+  const rows = document.querySelectorAll(".gametiles__row");
+
+  const currentRow: HTMLElement | undefined = findCurrentRow(rows);
+
+  insertLetterInCurrentRow(currentRow);
+
+  // answer.push(letter);
+}
+
+function insertLetterInCurrentRow(row: HTMLElement | undefined) {
+  console.log(row);
+}
+
+function findCurrentRow(rows: NodeListOf<Element>): HTMLElement | undefined {
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i] as HTMLElement;
+
+    const tilesAreEmpty = checkTilesEmpty(row);
+    // const emptyTile = checkTilesEmpty(row);
+
+    if (tilesAreEmpty) {
+      return row;
+    }
+  }
+}
+
+function checkTilesEmpty(row: HTMLElement): boolean | undefined {
+  const tiles = row.children;
+
+  for (let i = 0; i < tiles.length; i++) {
+    const tile = tiles[i];
+    const tileContent = tile.textContent;
+
+    if (tileContent === "") {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
